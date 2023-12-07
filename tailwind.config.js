c57dd390ea13e964abbdb7a188f845fe00e6ee44
aldipram <nandaaldi80@gmail.com>
2023-12-07 09:03:12 +0700

    update main

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBackground: '#0c111f',
+        brightColor: '#f9ec7d',
+      },
+    },
   },
   plugins: [],
 }
